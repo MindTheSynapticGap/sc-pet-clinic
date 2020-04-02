fa3f1652fa8012c43d27600bcd570a1f7552cc82
StefanoCasciarri <stefan.casciarri@gmail.com>
2020-04-02 02:01:59 +0200

    Initial cleanup

---
+++ src/main/java/com/casciarri/scpetclinic/ScPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.casciarri.scpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casciarri/scpetclinic/ScPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casciarri.scpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
